1caaabae3cf96aab436a9233d554ae924206b5d6
felipeEAC <@feilpeEAC>
2021-04-05 23:14:18 -0300

    Upcasting / Downcasting

---
+++ src/entties/BusinessAccount.java
@@ -0,0 +1,29 @@
+package entties;
+
+public class BusinessAccount extends Account{
+	
+	private Double loanLimit ;
+	
+	public BusinessAccount(){
+		super();
+	}
+
+	public BusinessAccount(Integer number, String holder, Double balance, Double loanLlimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLlimit;
+	}
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+
+	public void setLoanLlimit(Double loanLlimit) {
+		this.loanLimit = loanLimit;
+	}
+	public void loan(double amount) {
+		if (amount <= loanLimit) {
+			balance =+amount -10;
+		}
+	}
+	
+}
